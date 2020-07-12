fc736cbdadfe236fea5c58693e400942927cccbb
Rafael Badaró <rafabadaro.15@gmail.com>
2020-07-12 15:47:13 -0300

    colocando icones e escrevendo algumas ideias para o site

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <h1>Body</h1>
+    <h1>Um pouco sobre mim</h1>
+    <p>Oi!, meu nome é Rafael mas todo mundo me chama de Badaró por causa do meu sobrenome. Eu sou um estudante de engenharia de software na PUC</p>
+    <h1>Linguagens e tecnologias</h1>
+    <h1>Projetos  - criar um component de projetos e um link pro github</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Body',
-  
-}
+  name: "Body"
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="footer">
     <div class="socials">
-      <span>My socials:</span>
-      <a id="instagram" href="https://www.instagram.com/rafabadaro/">
-        <img src="./assets/instagram_logo.png" />
-      </a>
+      <span class="socials-header">You can find me at:</span>
+      <div class="socials-links">
+        <a class="social" id="instagram" href="https://www.instagram.com/rafabadaro/">
+          <img class="social-img" src="../assets/insta.png" />
+        </a>
+        <a class="social" id="linkedIn" href="https://www.linkedin.com/in/rafael-badaro/">
+          <img class="social-img linkedin" src="../assets/linkedin.png" />
+        </a>
+        <a class="social" id="github" href="https://github.com/RafaelBadaro">
+          <img class="social-img" src="../assets/github.png" />
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -16,5 +24,23 @@ export default {
 </script>
 
 <style scoped>
-
+.footer {
+  background-color: #cbd3f2;
+  text-align: start;
+}
+.socials{
+  margin-left: 50px;
+}
+.socials-header{
+  font-size: 22px;
+}
+.social-img {
+  width: 100px;
+  height: 100px;
+}
+.linkedin {
+  width: 80px;
+  height: 80px;
+  margin-bottom: 10px;
+}
 </style>
