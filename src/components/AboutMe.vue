abbba668245e4fa808c1ff9a2795593f9c2d06d1
Rafael Badaró <rafabadaro.15@gmail.com>
2020-07-20 18:15:55 -0300

    Criando projects e aboutme

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Oi! Meu nome é Rafael mas todo mundo me chama de Badaró por causa do meu sobrenome. Eu sou um estudante de engenharia de software na PUC</p>
+    <p>SoftwareEngineer rafael = new SoftwareEngineer()</p>
+    <ul>
+      <li>rafael.Idade = 21 anos</li>
+      <li>rafael.Curso = Engenharia de Software</li>
+      <li>rafael.PosicaoAtual = dev junior</li>
+      <li>
+        rafael.Trabalho = dti digital
+        <ul>
+          <li>rafael.Tempo = 2 anos</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutMe"
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <h1>Um pouco sobre mim</h1>
-    <p>Oi!, meu nome é Rafael mas todo mundo me chama de Badaró por causa do meu sobrenome. Eu sou um estudante de engenharia de software na PUC</p>
+    <AboutMe></AboutMe>
     <h1>Linguagens e tecnologias</h1>
-    <h1>Projetos  - criar um component de projetos e um link pro github</h1>
+    <h1>Projetos</h1>
+    <Projects></Projects>
   </div>
 </template>
 
 <script>
+import Projects from "../components/Projects.vue";
+import AboutMe from "../components/AboutMe.vue";
+
 export default {
-  name: "Body"
+  name: "Body",
+   components: {
+    Projects,
+    AboutMe
+  }
 };
 </script>
 
