e85f406b7f76f3f38e149524a9752144229371c7
Rafael Badaró <rafabadaro.15@gmail.com>
2021-02-23 23:59:59 -0300

    Trocando as redes sociais para ficar no header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,8 @@
 <template>
   <div class="footer">
-    <div class="socials">
-      <span class="socials-header">Redes sociais:</span>
-      <div class="socials-links">
-        <a class="social" id="instagram" href="https://www.instagram.com/rafabadaro/">
-          <img class="social-img" src="../assets/insta.png" />
-        </a>
-        <a class="social" id="linkedIn" href="https://www.linkedin.com/in/rafael-badaro/">
-          <img class="social-img linkedin" src="../assets/linkedin.png" />
-        </a>
-        <a class="social" id="github" href="https://github.com/RafaelBadaro">
-          <img class="social-img" src="../assets/github.png" />
-        </a>
-      </div>
-      <a class="waves-effect waves-light btn" href="/curriculo.pdf" download>
-        Baixar curriculo
-      </a>
-    </div>
+    <a class="waves-effect waves-light btn" href="/curriculo.pdf" download>
+      Baixar curriculo
+    </a>
   </div>
 </template>
 
@@ -29,24 +15,7 @@ export default {
 <style scoped>
 .footer {
   background-color: #70a0af;
-  text-align: start;
-}
-.socials {
-  margin-left: 20px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-}
-.socials-header {
-  font-size: 22px;
-}
-.social-img {
-  width: 100px;
-  height: 100px;
-}
-.linkedin {
-  width: 80px;
-  height: 80px;
-  margin-bottom: 10px;
+  padding-top: 13px;
+  padding-bottom: 9px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="header">
-    <h1>Rafael Badaró</h1>
-    <h4>Email: rafaelaraujobadaro@gmail.com</h4>
+    <h4>Contato: rafaelaraujobadaro@gmail.com</h4>
+    <div class="socials">
+      <div class="socials-links">
+        <a
+          class="social"
+          id="instagram"
+          href="https://www.instagram.com/rafabadaro/"
+        >
+          <img class="social-img" src="../assets/insta.png" />
+        </a>
+        <a
+          class="social"
+          id="linkedIn"
+          href="https://www.linkedin.com/in/rafael-badaro/"
+        >
+          <img class="social-img linkedin" src="../assets/linkedin.png" />
+        </a>
+        <a class="social" id="github" href="https://github.com/RafaelBadaro">
+          <img class="social-img" src="../assets/github.png" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
 };
 </script>
 
 <style scoped>
 .header {
   background-color: #70a0af;
+}
+.socials {
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.social-img {
+  width: 100px;
+  height: 100px;
+}
+.linkedin {
+  width: 80px;
+  height: 80px;
+  margin-bottom: 10px;
 }
 </style>
